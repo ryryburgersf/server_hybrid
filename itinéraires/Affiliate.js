0ca7c0dc6e079d2bc6d7141dc69f6be9332515d3
ryryburgersf <82973032+ryryburgersf@users.noreply.github.com>
2021-06-06 02:30:08 +0200

    Create Affiliate.js

---
+++ itinéraires/Affiliate.js
@@ -0,0 +1,14 @@
+const  express  =  require ( 'express' ) ;
+const  app  =  express ( ) ;
+
+application . get ( '/affiliate/api/public/affiliates/slug/:slug' ,  async  ( req ,  res )  =>  {
+    rés . json ( {
+			identifiant : '69420694206942069420694206942069' ,
+			limace : req . paramètres . Nom de l'affilié ,
+			displayName : req . paramètres . Nom de l'affilié ,
+			statut : 'ACTIF' ,
+			vérifié : vrai
+		} )
+} )
+
+module . exportations  =  application ;
