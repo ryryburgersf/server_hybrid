ee212c170d27f78bb008c58733f3e071ed344092
ryryburgersf <82973032+ryryburgersf@users.noreply.github.com>
2021-06-06 03:41:54 +0200

    Create Exchange.cs

---
+++ RyryburgerFN Launcher/Epic/Exchange.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace RyryburgerFN_Launcher.Epic
+{
+	public class Exchange
+	{
+		[JsonPropertyName("expiresInSeconds")]
+		public int ExpiresInSeconds { get; set; }
+
+		[JsonPropertyName("code")]
+		public string Code { get; set; }
+
+		[JsonPropertyName("creatingClientId")]
+		public string CreatingClientId { get; set; }
+	}
+}
