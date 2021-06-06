53810361e028a8d7c2eff3faaeec0c2f8b4e9926
ryryburgersf <82973032+ryryburgersf@users.noreply.github.com>
2021-06-06 02:47:46 +0200

    Create socialban.js

---
+++ itinéraires/socialban.js
@@ -0,0 +1,8 @@
+const  express  =  require ( 'express' ) ;
+const  app  =  express ( ) ;
+
+application . get ( '/socialban/api/public/v1/:any' ,  ( req ,  res )  =>  {
+    rés . statut ( 204 ) . fin ( ) ;
+} ) ;
+
+module . exportations  =  application ;
