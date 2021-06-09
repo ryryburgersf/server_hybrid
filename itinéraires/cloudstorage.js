a9e56ea5f4b952003a00e0bd0cf4325946c0f4b8
ryryburgersf <82973032+ryryburgersf@users.noreply.github.com>
2021-06-09 16:34:32 +0200

    Update cloudstorage.js

--- itinéraires/cloudstorage.js
+++ itinéraires/cloudstorage.js
@@ -1,49 +1,51 @@
-const  express  =  require ( 'express' ) ;
-const  app  =  express ( ) ;
-const  fs  =  require ( 'fs' ) ;
-const  crypto  =  require ( 'crypto' ) ;
-const  config  =  require ( '../config' ) ;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+const config = require('../config');
 
-// INSPIRÉ PAR AURORAFN & NEONITE
-application . get ( '/fortnite/api/cloudstorage/system' ,  ( req ,  res )  =>  {
-	laisser  résultat  =  [ ] ;
-	fs . readdirSync ( ` ${ config . répertoire } /cache/cloudstorage` )
-		. pourChaque ( ( fichier )  =>  {
-			résultat . pousser ( {
-				nomFichier unique : fichier ,
-				nom de fichier : fichier ,
-				hachage : crypto . createHash ( ' sha1' ) . mise à jour ( fichier ) . résumé ( 'hex' ) ,
-				hash256 : crypto . createHash ( 'sha256' ) . mise à jour ( fichier ) . résumé ( 'hex' ) ,
-				longueur : fichier . longueur ,
-				contentType : 'application/octet-stream' ,
-				téléchargé : fs . statSync (
-					` ${ config . répertoire } /cache/cloudstorage/ ${ fichier } `
-				) . mtime ,
-				Type de stockage : 'S3' ,
-				doNotCache : false ,
-			} ) ;
-		} ) ;
-	rés . json ( [ ] ) ;
-} ) ;
+// INSPIRED BY AURORAFN & NEONITE
+app.get('/fortnite/api/cloudstorage/system', (req, res) => {
+	let result = [];
+	fs.readdirSync(`${config.directory}/cache/cloudstorage`)
+		.forEach((file) => {
+			result.push({
+				uniqueFileName: file,
+				filename: file,
+				hash: crypto.createHash('sha1').update(file).digest('hex'),
+				hash256: crypto.createHash('sha256').update(file).digest('hex'),
+				length: file.length,
+				contentType: 'application/octet-stream',
+				uploaded: fs.statSync(
+					`${config.directory}/cache/cloudstorage/${file}`
+				).mtime,
+				storageType: 'S3',
+				doNotCache: false,
+			});
+		});
+	res.json(result);
+});
 
-application . get ( '/fortnite/api/cloudstorage/system/config' ,  ( req ,  res )  =>  {
-	rés . envoyer ( '' ) ;
-} ) ;
+app.get('/fortnite/api/cloudstorage/system/config', (req, res) => {
+	res.send('');
+});
 
-application . get ( '/fortnite/api/cloudstorage/system/:fileName' ,  ( req ,  res )  =>  {
-	si  (
-		fs . existeSync (
-			` ${ config . répertoire } /cache/cloudstorage/ ${ req . paramètres . nom_fichier } `
+app.get('/fortnite/api/cloudstorage/system/:fileName', (req, res) => {
+	if (
+		fs.existsSync(
+			`${config.directory}/cache/cloudstorage/${req.params.fileName}`
 		)
-	)  {
-		rés . envoyer (
-			fs . readFileSync (
-				` ${ config . répertoire } /cache/cloudstorage/ ${ req . paramètres . nom_fichier } `
+	) {
+		res.send(
+			fs.readFileSync(
+				`${config.directory}/cache/cloudstorage/${req.params.fileName}`
 			)
-		) ;
-	}  autre  {
-		rés . état ( 404 ) . fin ( ) ;
+		);
+	} else {
+		res.status(404).end();
 	}
-} ) ;
+});
 
-module . exportations  =  application ;
+module.exports = app;
+
